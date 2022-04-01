61f6dccd9b276cd296300f5c4973a09e8485c649
Miguel Alzate <alzate.jimenez.09@gmail.com>
2022-03-31 21:23:20 -0500

    Initial CORS settings

--- models/server.js
+++ models/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
 const path = require("path");
+const cors = require("cors");
 
 const Sockets = require("./sockets");
 
@@ -20,7 +21,11 @@ class Server {
   }
 
   middlewares() {
+    //Deploy public directory
     this.app.use(express.static(path.resolve(__dirname, "../public")));
+
+    //CORS
+    this.app.use(cors());
   }
 
   configSockets() {
